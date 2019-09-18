b8dd617196bdeb5c85c21da8f1180591704d5e9f
Nikolas Lencioni <nikolaslencioni@MacBook-Air-de-Nikolas.local>
2019-09-18 13:25:17 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Routes from './routes';
 
-// import './bootstrap.css'
+import './bootstrap.css'
 import './styles.css'
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
