dd514ceb4dde7eca0666b656075519a60023afa5
Nikolas Lencioni <nikolasagl@hotmail.com>
2019-09-26 17:15:59 -0300

    .

---
+++ src/pages/admin/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Admin() {
+
+    return (
+        <h1>AQUI ADMIN</h1>
+    )
+}
+
+export default Admin
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Home from './pages/home'
+import Admin from './pages/admin'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route exact path='/' component={Home} />
+            <Route exact path='/admin' component={Admin} />
         </BrowserRouter>
     )
 }
